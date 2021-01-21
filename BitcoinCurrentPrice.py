429cbabed45ab862b12c215210e8248991df322f
BUFFER4096 <77799060+BUFFER4096@users.noreply.github.com>
2021-01-21 18:03:16 +0100

    Add files via upload

---
+++ BitcoinCurrentPrice.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup as BS
+
+import time
+
+logo = """
+ _________    _______________    ____________
+|  _____  |  |	             |	|	     |							
+|  |   |  |  |_______________|	|     _______|
+|  |___|  |       |     |       |    |
+|_________|	  |	|	|    |
+|  |	          |     |       |    |
+|__|_______	  |	|	|    |
+|  _____  |	  |	|	|    | 
+|  |   |  |	  |	|	|    |_______
+|  |___|  |       |	|	|	     |
+|_________|	  |_____|	|____________|
+
+"""
+
+print(logo)
+
+check = 0
+
+while 1:
+    try:
+        print("Current BTC price: "+BS(requests.get("https://cryptowat.ch/it-it/").text, "html.parser").find("span", attrs={"class": "_3XNm6CSrchU-MNbu1Zh3m2"}).text, end="\r")
+    except:
+        pass
+    
+    if check == 0:
+        time.sleep(3)
+    else:
+        check = 0
+        
+
+
+
